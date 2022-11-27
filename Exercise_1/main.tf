2fa39abdbe5ea5c6aa2510082b7dfdac34696b87
DESKTOP-AN24RJL\nn <jossnghia@gmail.com>
2022-11-27 15:53:45 +0700

    add new

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,27 @@
 # TODO: Designate a cloud provider, region, and credentials
-
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-
+resource "aws_instance" "Udacity_T2" {
+  count = "4"
+  ami = "ami-0b0dcb5067f052a63"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Udacity T2"
+  }
+}
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
+# resource "aws_instance" "Udacity_M4" {
+#   count = "2"
+#   ami = "ami-0b0dcb5067f052a63"
+#   instance_type = "m4.large"
+#   tags = {
+#   Name = "Udacity M4"
+#  }
+#}
+
+#terraform destroy -target aws_instance.<NAME>
